fcd23558deda14607cc78dbeb9c95e5b535a3066
JOSUE DIAZ <josue.diaz@epn.edu.ec>
2021-03-11 09:43:31 -0500

    pico y placa logic and final design touches

--- src/app/validator/travel-validator.service.ts
+++ src/app/validator/travel-validator.service.ts
@@ -21,7 +21,62 @@ export class TravelValidatorService {
     var lastDigit: number = +data.license.substr(data.license.length - 1);
     var day = new Date(data.date).getDay();
     var time = data.time
+  
+    if ((time >= "07:00" && time <= "09:30") || (time >= "16:00" && time <= "19:30")) 
+    {
+      switch (day) {
+        case 1:
 
-    console.log(time)
+            if (lastDigit == 1 || lastDigit == 2) {
+              this.failureAlert();
+            }
+            else {
+              this.confirmationAlert();
+            }
+            break;
+        case 2:
+
+            if (lastDigit == 3 || lastDigit == 4) {
+              this.failureAlert();
+            }
+            else {
+              this.confirmationAlert();
+            }
+            break;
+        case 3:
+
+            if (lastDigit == 5 || lastDigit == 6) {
+              this.failureAlert();
+            }
+            else {
+              this.confirmationAlert();
+            }
+            break;
+
+        case 4:
+
+            if (lastDigit == 7 || lastDigit == 8) {
+              this.failureAlert();
+            }
+            else {
+              this.confirmationAlert();
+            }
+            break;
+
+        case 5:
+
+            if (lastDigit == 0 || lastDigit == 9) {
+              this.failureAlert();
+            }
+            else {
+              this.confirmationAlert();
+            }
+            break;
+        default:
+          this.confirmationAlert();
+        }
+    }else{
+      this.confirmationAlert();
+    }
   }
 }
